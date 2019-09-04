65e5c00ec7abb4d0666115e5efffdc28f1441c29
zouhair.elhadi <zouhair11elhadi@gmail.com>
2019-09-04 13:00:38 +0000

    commit architecture

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='lists-shops-nearby',
+    version='0.0.1',
+    packages=['backend.api', 'backend.core', 'backend.tests', 'backend.commun'],
+    url='',
+    license='',
+    author='Elhadi',
+    author_email='zouhair.elhadi@aiehtp.net',
+    description=''
+)
